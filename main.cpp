b497126f2fdfdad261d345032cb14176dce6b38f
chuanky <shicq01@gmail.com>
2020-06-07 22:51:12 +0800

    添加FactoryMethod,AbstractFactory和Builder Pattern

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "AbstractFactory.h"
+#include "FactoryMethod.h"
+#include "BuilderPattern.h"
+
+int main() {
+	//main_abstract_factory();
+	//main_factory_method();
+	main_builder_pattern();
+}
