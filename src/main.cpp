1121d7c29e466442e3fd872ec8ddb563cc4f0a93
chuanky <shicq01@gmail.com>
2020-06-08 16:13:31 +0800

    加入Singleton Pattern

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include "FactoryMethod.h"
 #include "BuilderPattern.h"
 #include "Prototype.h"
+#include "Singleton.h"
 
 int main() {
 	//main_abstract_factory();
 	//main_factory_method();
 	//main_builder_pattern();
-	main_prototype();
+	//main_prototype();
+	main_singleton();
 }
