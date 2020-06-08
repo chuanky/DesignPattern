6fa39d1d7d7679a589a9f9ed022ebb9c1514beb1
chuanky <shicq01@gmail.com>
2020-06-08 15:00:11 +0800

    调整项目文件夹结构,添加Prototype Pattern

---
+++ src/Prototype.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+#include <unordered_map>
+
+int main_prototype();
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "AbstractFactory.h"
 #include "FactoryMethod.h"
 #include "BuilderPattern.h"
+#include "Prototype.h"
 
 int main() {
 	//main_abstract_factory();
 	//main_factory_method();
-	main_builder_pattern();
+	//main_builder_pattern();
+	main_prototype();
 }
