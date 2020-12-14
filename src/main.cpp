69a7b419c99ebb28a36bee87a6b38ff16ac35064
chuanky <shicq01@gmail.com>
2020-12-14 21:57:03 +0800

    add Log class

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,41 @@
-#include "AbstractFactory.h"
-#include "FactoryMethod.h"
-#include "BuilderPattern.h"
-#include "Prototype.h"
-#include "Singleton.h"
+#include <iostream>
+
+class Log {
+public:
+	int errorLevel = 1;
+	int warningLevel = 2;
+	int infoLevel = 3;
+private:
+	int m_logLevel;
+public:
+	void Error(const char* message) {
+		if (m_logLevel >= errorLevel) {
+			std::cout << "[Error]: " << message << std::endl;
+		}
+	}
+
+	void Warning(const char* message) {
+		if (m_logLevel >= warningLevel) {
+			std::cout << "[Warning]: " << message << std::endl;
+		}
+	}
+
+	void Info(const char* message) {
+		if (m_logLevel >= infoLevel) {
+			std::cout << "[Info]: " << message << std::endl;
+		}
+	}
+
+	void SetLogLevel(int level) {
+		m_logLevel = level;
+	}
+};
 
 int main() {
-	//main_abstract_factory();
-	//main_factory_method();
-	//main_builder_pattern();
-	//main_prototype();
-	main_singleton();
+	Log log;
+	log.SetLogLevel(log.errorLevel);
+
+	log.Info("logging...");
+	log.Warning("logging...");
+	log.Error("logging");
 }
